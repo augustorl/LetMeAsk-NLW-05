8f1dd80a1db3d4b3d032cda0718d5a783c7cf960
augustorl <gutin22@gmail.com>
2021-06-23 22:00:58 -0300

    add: Room Route path & Finis Day 3!

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/Home";
 
 import { AuthContextProvider } from './contexts/AuthContext'
 import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <AuthContextProvider>
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" exact component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
       </AuthContextProvider>
     </BrowserRouter>
   );
