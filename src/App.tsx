a9824cf265754decf0476d7f03c8057bad705367
augustorl <gutin22@gmail.com>
2021-06-23 21:53:20 -0300

    add: newRoom to routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route,  BrowserRouter } from 'react-router-dom'
 import { Home } from "./pages/Home";
 
 import { AuthContextProvider } from './contexts/AuthContext'
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" exact component={NewRoom} />
       </AuthContextProvider>
     </BrowserRouter>
   );
