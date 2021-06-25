947b3427bbd4fdbf1b62fd933e61df205266e0e6
augustorl <gutin22@gmail.com>
2021-06-25 00:44:04 -0300

    update: app Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Route,  BrowserRouter, Switch } from 'react-router-dom'
 import { AuthContextProvider } from './contexts/AuthContext'
 
 import { Home } from "./pages/Home";
-import { Room } from './pages/Room';
-import { NewRoom } from "./pages/NewRoom";
-import { AdminRoom } from './pages/AdminRoom';
 import { CustomThemeProvider } from './contexts/ThemeProvider';
 import { GlobalStyle } from './styles/GlobalStyles';
+import { NewRoom } from './pages/Rooms/NewRoom';
+import { Room } from './pages/Rooms/Room';
+import { AdminRoom } from './pages/Rooms/AdminRoom';
 
 function App() {
   return (
