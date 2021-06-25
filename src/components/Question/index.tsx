c82ef0b4e1dada132a6cdf800519f3afa18ddf57
augustorl <gutin22@gmail.com>
2021-06-24 22:09:32 -0300

    add: classNames to question

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from 'react'
+import { ReactNode } from 'react';
+import cx from 'classnames'
 
 import './styles.scss'
 
@@ -13,9 +14,21 @@ type QuestionProps = {
   isHighlighted?: boolean, 
 }
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({ 
+  content, 
+  author, 
+  isAnswered = false, 
+  isHighlighted = false, 
+  children 
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div 
+      className={cx(
+        'question', 
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered},
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
@@ -28,4 +41,4 @@ export function Question({ content, author, children }: QuestionProps) {
       </footer>
     </div>
   )
-} 
+}
