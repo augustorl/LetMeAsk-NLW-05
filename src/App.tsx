df4a17745c556d9006163092c8529aedab27ca1a
augustorl <gutin22@gmail.com>
2021-06-24 21:37:58 -0300

    update: Routes & finish lecture 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { Route,  BrowserRouter } from 'react-router-dom'
-
-import { Home } from "./pages/Home";
+import { Route,  BrowserRouter, Switch } from 'react-router-dom'
 
 import { AuthContextProvider } from './contexts/AuthContext'
-import { NewRoom } from './pages/NewRoom';
+
+import { Home } from "./pages/Home";
 import { Room } from './pages/Room';
+import { NewRoom } from "./pages/NewRoom";
+import { AdminRoom } from './pages/AdminRoom';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" exact component={NewRoom} />
-        <Route path="/rooms/:id" component={Room} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
